d72c3b6b129840dc43d35008179c1b8f914be91e
vianho <silviana.h016@gmail.com>
2021-12-12 23:50:21 +0800

    adjust template to habits

--- day template/main.py
+++ day template/main.py
@@ -1,10 +1,10 @@
 from utils import read_file, transform_input, get_logger
-LOGGER = get_logger(level="INFO")
+LOGGER = get_logger(level="DEBUG")
 # from dayx_1 import sol1
 # from dayx_2 import sol2
 
 def main():
-    filename = 'input.txt'
+    filename = 'test.txt'
     data = transform_input(read_file(filename))
     LOGGER.info(f'{len(data)=}')
     LOGGER.debug(f'{data[:10]=}')
